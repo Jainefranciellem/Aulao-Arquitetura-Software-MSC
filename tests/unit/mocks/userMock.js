2c963728e9376a07fc80f5ee92cc7636646cf734
Jainefranciellem <jainefraciellen@gmail.com>
2023-05-26 13:36:24 -0300

    Subindo alguns testes!

---
+++ tests/unit/mocks/userMock.js
@@ -0,0 +1,23 @@
+const mockGetAll = [
+  {
+    "id": 1,
+    "name": "João",
+    "email": "joao@example.com"
+  },
+  {
+    "id": 2,
+    "name": "Maria",
+    "email": "maria@example.com"
+  },
+]
+
+const mockGetById = {
+  "id": 1,
+  "name": "João",
+  "email": "joao@example.com"
+};
+
+module.exports = {
+  mockGetAll,
+  mockGetById
+}
